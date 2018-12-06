c1a9b08ca946aaa44700f1cf2365e12b79a266e4
Shashank Kulkarni <shanky.kulkarni@gmail.com>
2018-12-06 21:14:50 +0530

    Started work on Hex File Parser app

---
+++ Sample Applications/Hex File Parser/helper.py
@@ -0,0 +1,6 @@
+class hex_record_type:
+	def __init__(self):
+		record_data = '00'
+		record_eof = '01'
+		record_ext_seg_add = '02'
+		record_ext_lin_add = '04'
---
+++ Sample Applications/Hex File Parser/hex-file-parser.py
@@ -0,0 +1,7 @@
+import os
+import sys
+from helper import *
+
+hexRecordType = hex_record_type()
+
+print(hexRecordType)
